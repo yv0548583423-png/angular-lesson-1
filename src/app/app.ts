c081988c5a87531ead7a82a4338a043afc124ef6
unknown <you@example.com>
2026-02-08 23:50:13 +0200

    added age function and inline styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,9 @@ import { IdCard } from './id-card/id-card';
   styleUrl: './app.scss'
 })
 export class App {
-
+  people = [
+    { birthDate: new Date(1995, 4, 15) },
+    { birthDate: new Date(1998, 10, 20) },
+    { birthDate: new Date(2000, 1, 5) }
+  ];
 }
